0b7a92a3346d422ffe8147d14818df36afff950e
Rahmat M. Samik-Ibrahim <19358416+rms46@users.noreply.github.com>
2023-02-17 08:32:59 +0700

    typechange: index.md; new file: links.md tips.md; modified:   mylog.txt myscript.sh

--- TXT/myscript.sh
+++ TXT/myscript.sh
@@ -1,13 +1,20 @@
 #!/bin/bash
-# Copyright (C) 2020-2022 Cicak Bin Kadal
-# This free script is distributed in the hope that it will be 
+# Copyright (C) 2020-2023 Cicak Bin Kadal
+
+# This free document is distributed in the hope that it will be 
 # useful, but WITHOUT ANY WARRANTY; without even the implied 
 # warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
 
-# REV03: Tue 11 Oct 2022 06:00
-# REV02: Wed 14 Sep 2022 08:00
-# REV01: Mon 12 Sep 2022 17:00
+# REV30: Fri 17 Feb 2023 08:00
+# REV29: Mon 13 Feb 2023 13:30
+# REV19: Sun 05 Feb 2023 20:00
+# REV11: Sun 08 May 2022 06:00
+# REV02: Sun 19 Sep 2021 15:00
 # START: Mon 28 Sep 2020 21:00
+
+# ATTN:
+# You new to set "REC2" with your own Public-Key Identity!
+# Check it out with "gpg --list-key"
 
 # ATTN:
 # You new to set "REC2" with your own Public-Key Identity!
@@ -70,7 +77,7 @@ case "$response" in
         ;;
     *)
         echo "It is not Week $WEEK!"
-        exit 1
+        usage
         ;;
 esac
 
@@ -89,7 +96,7 @@ for II in W?? ; do
 done
 popd
 
-if [[ "$WEEK" != "W00" ]] && [[ "$WEEK" != "W01" ]] ; then
+if [[ "$WEEK" != "W00" ]] ; then
     II="${RESDIR}my$WEEK.tar.bz2.asc"
     echo "Check and move $II..."
     [ -f $II ] && mv -vf $II .
--- index.md
+++ index.md
@@ -1,1 +1,10 @@
-README.md
+---
+---
+# os231
+
+[GitHub](https://github.com/cbkadal/os231/) | | [LOG](TXT/mylog.txt) | | [LINKS](LINKS/) || [TARBALL](SandBox/cbkadal.tar.xz)
+
+## May the fork() be with you!
+
+#### Rev 007 --- 17 Feb 2023
+
